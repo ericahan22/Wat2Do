2507e0a002520d9ea9b160d835c7c76ef58a9962
Tony Qiu <tonyqiu@Mac-mini.local>
2025-09-24 15:07:41 -0400

    remove transition in tabs

--- frontend/src/components/ui/tabs.tsx
+++ frontend/src/components/ui/tabs.tsx
@@ -27,7 +27,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-gray-900 data-[state=active]:shadow dark:focus-visible:ring-gray-300 dark:data-[state=active]:bg-gray-900 dark:data-[state=active]:text-gray-50",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-gray-900 data-[state=active]:shadow dark:focus-visible:ring-gray-300 dark:data-[state=active]:bg-gray-900 dark:data-[state=active]:text-gray-50",
       className
     )}
     {...props}
