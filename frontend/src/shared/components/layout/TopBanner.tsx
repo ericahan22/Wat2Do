b7cb96698f3cfafe214889e2d63e5d400ab96d46
Tony Qiu <tonyqiu@Mac-mini.local>
2025-11-16 23:31:04 -0500

    topbaner text mentions events scraped in real time

--- frontend/src/shared/components/layout/TopBanner.tsx
+++ frontend/src/shared/components/layout/TopBanner.tsx
@@ -3,23 +3,23 @@ import { X } from "lucide-react";
 import { IconButton } from "@/shared/components/ui/icon-button";
 import { formatEventDate } from "@/shared/lib/dateUtils";
 import { useLocalStorage } from "react-use";
+import { useNavigate } from "react-router-dom";
 
 function TopBanner() {
   const [isVisible, setIsVisible] = useLocalStorage("topBannerVisible", true);
 
+  const navigate = useNavigate();
   if (!isVisible) return null;
 
   return (
     <div className="w-full !bg-[#0056D6]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-1.5 relative flex items-center justify-center">
         <a
-          href="https://wat2do.instatus.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-gray-200"
+          onClick={() => navigate("/auth-sign-in")}
+          className="hover:text-gray-200 cursor-pointer"
         >
           <span className="text-sm font-small text-center !text-white">
-            {formatEventDate("2025-11-06")} - Interested events now available!
+            {formatEventDate("2025-11-16")} - Events added in real time!
           </span>
         </a>
         <IconButton
